f6273b5731c9393be7f4d20cc06f4ded3257631d
sandipan898 <sandipan.das898@gmail.com>
2020-07-31 08:42:52 +0530

    adding dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { MenuItem, Select, FormControl } from "@material-ui/core";
 import "./App.css";
 
+// function renderDropdown(countries) {
+//   return countriesList;
+// }
+
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
   return (
     <div className="app">
-      <h1>COVID-19 Tracker</h1>
       {/* Header */}
       {/* Title + Select input dropdown Menu */}
       {/* InfoBoxes */}
       {/* Table */}
       {/* Graph */}
       {/* Map */}
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries and how a dropdown list */}
+            {countries.map(country => (
+              <MenuItem value="worldwide">{country}</MenuItem>
+            ))}
+            ;
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
